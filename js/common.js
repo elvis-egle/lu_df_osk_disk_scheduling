cac0e589252c99ac840fd1be64027167a8a0a0d9
Azeroc <azeroc2601@gmail.com>
2017-03-24 19:17:59 +0200

    Added starting track input, finished queue input logic, added bit of validation and finished config data function (which will be used to pass config data to animation scripts)

--- js/common.js
+++ js/common.js
@@ -5,3 +5,46 @@ function round(x, n) {
     var dec = Math.pow(10, n);
     return Math.round(x * dec) / dec;
 }
+
+function maxInArray(a) {
+    if ($.isArray(a) && a.length > 0) {
+        var index;
+        var max = a[0];
+        var maxIndex = 0;
+        for (index = 1; index < a.length; ++index) {
+            if (a[index] > max) {
+                max = a[index];
+                maxIndex = index;
+            }
+        }
+
+        return { "value": max, "index": maxIndex };
+    } else {
+        return { "value": 0, "index": -1 };
+    }
+}
+
+function minInArray(a) {
+    if ($.isArray(a) && a.length > 0) {
+        var index;
+        var min = a[0];
+        var minIndex = 0;
+        for (index = 1; index < a.length; ++index) {
+            if (a[index] < min) {
+                min = a[index];
+                minIndex = index;
+            }
+        }
+
+        return { "value": min, "index": minIndex };
+    } else {
+        return { "value": 0, "index": -1 };
+    }
+}
+
+/// Bootstrap js extensions
+$(function() {
+    $("[data-hide]").on("click", function() {
+        $("." + $(this).attr("data-hide")).hide();
+    });
+});
--- js/main.js
+++ js/main.js
@@ -25,6 +25,10 @@ function uiInit() {
     $("#btnPause").click(pauseAnimation);
     $("#btnContinue").click(continueAnimation);
     $("#btnReset").click(resetAnimation);
+    $("#btnGenerateQueue").click(generateSeekPositions);
+
+    // Hide alerts
+    $("#alertQueueError").hide();
 
     // Make sure number inputs are fixed after inputing value
     $("#inputTrackSize").change(function() {
@@ -35,10 +39,20 @@ function uiInit() {
         }
     });
     $("#inputGenerateCount").change(function() {
-        var trackSize = $("#inputGenerateCount").val();
+        var genCount = $("#inputGenerateCount").val();
 
-        if (trackSize < 1) {
+        if (genCount < 1) {
             $("#inputGenerateCount").val("1");
+        }
+    });
+    $("#inputStartingTrack").change(function() {
+        var trackSize = $("#inputTrackSize").val();
+        var startingTrack = $("#inputStartingTrack").val();
+
+        if (startingTrack < 0) {
+            $("#inputStartingTrack").val("0");
+        } else if (startingTrack >= trackSize) {
+            $("#inputStartingTrack").val(trackSize - 1);
         }
     });
 
@@ -50,11 +64,26 @@ function startAnimation() {
     console.log("[DEBUG] startAnimation() - begin");
     console.log("[DEBUG] startAnimation() - TrackSize: " + $("#inputTrackSize").val());
 
+    // Hide previous alerts, so fade-in anim always play
+    $("#alertQueueError").hide();
+
     // Lock configuration UI
     configurationLock(true);
 
     // Lock button states before handling state transition
     animationControlsLock(true, true, true, true);
+
+    // Do data validation
+    if (!validateQueue()) {
+        // Enable queue error message
+        $("#alertQueueError").alert();
+        $("#alertQueueError").fadeIn();
+
+        // Reset controls and unlock configuration 
+        animationControlsLock(false, true, true, true);
+        configurationLock(false);
+        return;
+    }
 
     // Get config data if needed
     var data = getConfigData();
@@ -129,13 +158,21 @@ function resetAnimation() {
 // Lock/Disable or Unlock/Enable configuration inputs
 function configurationLock(lock) {
     if (lock) {
-        $("#algorithmSelect").prop("disabled", true);
-        $("#algorithmSelect").selectpicker("refresh");
         $("#inputTrackSize").prop("disabled", true);
+        $("#inputStartingTrack").prop("disabled", true);
+        $("#inputSeekPositionQueueGenSelect").prop("disabled", true);
+        $("#inputSeekPositionQueueGenSelect").selectpicker("refresh");
+        $("#inputGenerateCount").prop("disabled", true);
+        $("#btnGenerateQueue").prop("disabled", true);
+        $("#inputSeekPositionQueue").prop("disabled", true);
     } else {
-        $("#algorithmSelect").prop("disabled", false);
-        $("#algorithmSelect").selectpicker("refresh");
         $("#inputTrackSize").prop("disabled", false);
+        $("#inputStartingTrack").prop("disabled", false);
+        $("#inputSeekPositionQueueGenSelect").prop("disabled", false);
+        $("#inputSeekPositionQueueGenSelect").selectpicker("refresh");
+        $("#inputGenerateCount").prop("disabled", false);
+        $("#btnGenerateQueue").prop("disabled", false);
+        $("#inputSeekPositionQueue").prop("disabled", false);
     }
 }
 
@@ -147,23 +184,104 @@ function animationControlsLock(lockStart, lockPause, lockContinue, lockReset) {
     $("#btnReset").prop("disabled", lockReset);
 }
 
+function generateSeekPositions() {
+    var mode = $("#inputSeekPositionQueueGenSelect").selectpicker("val");
+    var genCount = $("#inputGenerateCount").val();
+    var trackSize = $("#inputTrackSize").val();
+
+    var positions = [];
+
+    var newPos;
+    var step;
+    var backAndForthFlag = 0;
+
+    // Generate array
+    for (step = 0; step < genCount; step++) {
+        if (mode == "unsorted") {
+            newPos = Math.floor(Math.random() * trackSize);
+            positions.push(newPos);
+        } else if (mode == "sorted") {
+            newPos = Math.floor(Math.random() * trackSize);
+            positions.push(newPos);
+
+            if (step + 1 >= genCount) {
+                positions.sort(function(a, b) { return a - b; });
+            }
+        } else if (mode == "rsorted") {
+            newPos = Math.floor(Math.random() * trackSize);
+            positions.push(newPos);
+
+            if (step + 1 >= genCount) {
+                positions.sort(function(a, b) { return b - a; });
+            }
+        } else if (mode == "backnforth") {
+            newPos = Math.floor(Math.random() * (trackSize / 2 - 1)) + (trackSize / 2) * backAndForthFlag;
+            backAndForthFlag = ~backAndForthFlag & 1; // Flip bit
+            positions.push(newPos);
+        }
+    }
+
+    // Append array to queue input
+    if ($("#inputSeekPositionQueue").val().trim().length > 0) {
+        $("#inputSeekPositionQueue").val(function() {
+            return this.value + ", " + positions.join(", ");
+        });
+    } else {
+        $("#inputSeekPositionQueue").val(function() {
+            return positions.join(", ");
+        });
+    }
+}
+
+function validateQueue() {
+    // Prepare for checks
+    var trackSize = $("#inputTrackSize").val();
+    var content = $("#inputSeekPositionQueue").val();
+    content = content.replace(/\s+/g, "");
+    content = content.replace(/,$/g, "");
+
+    // Check format
+    var re = new RegExp(/^\d+(,\d+)*$/);
+    if (!re.test(content)) {
+        return false;
+    }
+
+    // Check values
+    var positions = content.split(",");
+    var index;
+    for (index = 0; index < positions.length; index++) {
+        if (parseInt(positions[index]) > trackSize) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 // Returns simple JSON object of configuration data
 function getConfigData() {
     var data = {};
-    // Fetch algorithm. Possible values:
-    //  "fcfs" - FCFS (First Come, First Served)
-    //  "sstf" - SSTF (Shortest Seek Time First)
-    //  "scan" - Elevator (SCAN)
-    //  "cscan" - Circular SCAN (C-SCAN)
-    //  "look" - LOOK
-    //  "clook" - C-LOOK
-    data.algorithm = $("#algorithmSelect").selectpicker("val");
 
     // Fetch track size
     data.trackSize = $("#inputTrackSize").val();
 
+    // Fetch track starting position
+    data.trackStart = $("#inputStartingTrack").val();
+
     // Queue
-    data.seekQueue = [1, 9, 2, 8, 3, 7, 4, 6, 5]; // TODO: Finish queue ui input logic
+    data.seekQueue = [];
+    var queueInputValues = $("#inputSeekPositionQueue").val();
+    queueInputValues = queueInputValues.replace(/\s+/g, "");
+    queueInputValues = queueInputValues.replace(/,$/g, "");
+    var positionStrings = queueInputValues.split(",");
+
+    var index;
+    for (index = 0; index < positionStrings.length; index++) {
+        data.seekQueue.push({
+            "index": index,
+            "pos": parseInt(positionStrings[index])
+        });
+    }
 
     return data;
 }
